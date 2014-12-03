febfb62747ac062833ef5851a0285ce18b8cf899
mikrokupusar <ilicigor@gmail.com>
2014-12-03 23:19:46 +0100

    sledece je chart

---
+++ aspNetBootstrapDemo/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/aspNetBootstrap.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="aspNetBootstrapDemo.Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <br />
+    <br />
+    <div class="col-lg-11" runat="server">
+        <form runat="server">
+            <asp:GridView ID="gv3" CssClass="table" runat="server" BackColor="#CCCCCC"  AllowPaging="True" AllowSorting="True" BorderColor="#999999" BorderStyle="Solid" BorderWidth="3px" CellPadding="4" CellSpacing="2" ForeColor="Black">
+                <FooterStyle BackColor="#CCCCCC" />
+                <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#CCCCCC" ForeColor="Black" HorizontalAlign="Left" />
+                <RowStyle BackColor="White" />
+                <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+                <SortedAscendingCellStyle BackColor="#F1F1F1" />
+                <SortedAscendingHeaderStyle BackColor="#808080" />
+                <SortedDescendingCellStyle BackColor="#CAC9C9" />
+                <SortedDescendingHeaderStyle BackColor="#383838" />
+            </asp:GridView>
+            <br />
+            <asp:Button ID="btnPosebno" class="btn" runat="server" Text="Pregled po igraču" OnClick="btnPosebno_Click" />
+            <asp:DropDownList ID="ddlPlayers" CssClass="dropdown" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlPlayers_SelectedIndexChanged" ViewStateMode="Enabled"></asp:DropDownList>
+            <br />
+            <asp:GridView ID="GridView2" CssClass="table" runat="server" AllowPaging="true" AllowSorting="true"></asp:GridView>
+            
+        </form>
+    </div>
+    <div class="col-lg-1">
+        
+    </div>
+
+</asp:Content>
---
+++ aspNetBootstrapDemo/Default.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SQLite;
+using System.Data;
+using System.Configuration;
+
+namespace aspNetBootstrapDemo
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           PopulateGrid1();
+            
+            //PopulateGrid2();
+        }
+
+
+        protected void PopulateGrid1()
+        {
+            String connectionString = "Data Source=" + Server.MapPath(@"~\AppData\vukovi2015.db") + "; Version=3;";
+            String selectCommand = "select ime Ime, prezime Prezime, pozicija Pozicija, datum [Datum testiranja], benchTezina [Tezina bencha], bench Bench, deadliftTezina [Tezina mrtvog dizanja], deadlift [Mrtvo dizanje], broadjump [Skok u dalj] from Testiranje t join Bench b on b.benchId = t.benchId join Deadlift d on d.deadliftId=t.deadliftId join Players p on p.playersId=t.playersId join Pozicija poz on poz.pozicijaId=p.pozicijaId";
+
+
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(selectCommand, connectionString);
+
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds);
+
+            gv3.DataSource = ds;
+            gv3.DataBind();
+        }
+
+
+        protected void PopulateDDL()
+        {
+            DataTable players = new DataTable();
+            String connectionString = "Data Source=" + Server.MapPath(@"~\AppData\vukovi2015.db") + "; Version=3;";
+            String selectCommand = "select prezime, playersId from Players";
+            using(SQLiteConnection con = new SQLiteConnection(connectionString))
+            {
+                try
+                {
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(selectCommand, con);
+                    da.Fill(players);
+                    ddlPlayers.DataSource = players;
+                    ddlPlayers.DataTextField = "prezime";
+                    ddlPlayers.DataValueField = "playersId";
+                    ddlPlayers.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    string greska = ex.Message;
+                }
+            }
+
+
+
+            //SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(selectCommand, connectionString);
+
+            //DataSet ds = new DataSet();
+            //dataAdapter.Fill(ds);
+
+            //ddlPlayers.DataSource = ds;
+            //ddlPlayers.DataBind();
+
+        }
+
+        protected void PopulateGrid2()
+        {
+            String connectionString = "Data Source=" + Server.MapPath(@"~\AppData\vukovi2015.db") + "; Version=3;";
+            String selectCommand = "select datum [Datum testiranja], benchTezina [Tezina bencha], bench Bench, deadliftTezina [Tezina mrtvog dizanja], deadlift [Mrtvo dizanje], broadjump [Skok u dalj] from Testiranje t join Bench b on b.benchId = t.benchId join Deadlift d on d.deadliftId=t.deadliftId where playersId ="+ddlPlayers.SelectedValue.ToString();
+
+            
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(selectCommand, connectionString);
+
+            DataSet ds = new DataSet();
+            dataAdapter.Fill(ds);
+
+            GridView2.DataSource = ds;
+            GridView2.DataBind();
+        }
+
+        protected void ddlPlayers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateGrid2();
+        }
+
+        protected void btnPosebno_Click(object sender, EventArgs e)
+        {
+            PopulateDDL();
+            ddlPlayers.Visible = true;
+        }
+    }
+}
